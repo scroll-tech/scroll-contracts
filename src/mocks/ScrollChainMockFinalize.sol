7ba07fe0ce549967ef07d203187e0b220cfb0a1d
zimpha <zimpha@gmail.com>
2024-07-02 15:45:52 +0800

    feat: add finalizeBundle

--- src/mocks/ScrollChainMockFinalize.sol
+++ src/mocks/ScrollChainMockFinalize.sol
@@ -77,4 +77,25 @@ contract ScrollChainMockFinalize is ScrollChain {
 
         _afterFinalizeBatch(_totalL1MessagesPoppedOverall, _batchIndex, _batchHash, _postStateRoot, _withdrawRoot);
     }
+
+    /// @notice Finalize bundle without proof, See the comments of {ScrollChain-finalizeBundleWithProof}.
+    function finalizeBundle(
+        bytes calldata _batchHeader,
+        bytes32 _postStateRoot,
+        bytes32 _withdrawRoot
+    ) external OnlyProver whenNotPaused {
+        if (_postStateRoot == bytes32(0)) revert ErrorStateRootIsZero();
+
+        // compute pending batch hash and verify
+        (, bytes32 _batchHash, uint256 _batchIndex, ) = _loadBatchHeader(_batchHeader);
+        if (_batchIndex <= lastFinalizedBatchIndex) revert ErrorBatchIsAlreadyVerified();
+
+        // store in state
+        // @note we do not store intermediate finalized roots
+        lastFinalizedBatchIndex = _batchIndex;
+        finalizedStateRoots[_batchIndex] = _postStateRoot;
+        withdrawRoots[_batchIndex] = _withdrawRoot;
+
+        emit FinalizeBatch(_batchIndex, _batchHash, _postStateRoot, _withdrawRoot);
+    }
 }
